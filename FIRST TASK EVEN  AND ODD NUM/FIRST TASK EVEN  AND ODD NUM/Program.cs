3e3b6935884fb7851121ddba547d896f88ce074c
SHEFDAN <110844468+SHEFDAN@users.noreply.github.com>
2022-08-19 08:31:33 -0700

    Add files via upload

---
+++ FIRST TASK EVEN  AND ODD NUM/FIRST TASK EVEN  AND ODD NUM/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FIRST_TASK_EVEN__AND_ODD_NUM
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Even number from 1 to 35");
+            for (int i = 1; i <= 35; i++)
+            {
+                if (i % 2 == 0)
+
+                {
+                    Console.WriteLine(i);
+                }
+
+            }
+            
+            Console.WriteLine("odd number from 1 to 35");
+            for (int i = 1; i <= 35; i++)
+            {
+                if (i % 2 != 0)
+
+                {
+                    Console.WriteLine(i);
+
+                }
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
